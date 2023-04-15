54eb6b4cbc6caff3a8fed21bcbe196f44a576210
Ernest <em123nest@gmail.com>
2023-04-15 14:07:56 +0100

    problem solving commit

--- src/level_1/Number_Of_Occurrences.java
+++ src/level_1/Number_Of_Occurrences.java
@@ -1,4 +1,23 @@
 package level_1;
 
 public class Number_Of_Occurrences {
+    public static void findOccurrences() {
+        String word = "MISSISSIPPI";
+        int countForS = 0;
+        int countForI = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 's') {
+                countForS++;
+            }
+            else if (word.charAt(i) == 'i') {
+                countForI++;
+            }
+        }
+        System.out.println("Number of 's': " + countForS);
+        System.out.println("Number of 'i': " + countForI);
+    }
+
+    public static void main(String[] args) {
+        findOccurrences();
+    }
 }
