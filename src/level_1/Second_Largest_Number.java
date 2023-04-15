0bdfa6b9b5486442a1902a7f17c066efb28b94ea
Ernest <em123nest@gmail.com>
2023-04-15 02:31:23 +0100

    problem solving commit

---
+++ src/level_1/Second_Largest_Number.java
@@ -0,0 +1,10 @@
+package level_1;
+
+import java.util.Scanner;
+
+public class Second_Largest_Number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+}
